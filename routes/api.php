c589645ca2434a2263bc95a6b697e7d01e23bd01
Alejandro Burbano <Julianbur16@gmail.com>
2023-03-30 01:24:08 -0500

    cors

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,5 @@ Route::get('clients/{client}',[ClientController::class, 'show']);
 Route::put('clients/{client}',[ClientController::class, 'update']);
 Route::delete('clients/{client}',[ClientController::class, 'destroy']);
 
-Route::middleware(['cors'])->group(function () {
-    Route::post('/hogehoge', 'Controller@hogehoge');
-});
+
 
