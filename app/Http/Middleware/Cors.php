c990a9bf7e3af0e552c559defb58537c75534da9
Alejandro Burbano <Julianbur16@gmail.com>
2023-03-30 01:43:18 -0500

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,9 @@ class Cors
     {
         $response=$next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, x-requested-with, Application');
         return $response;
+       
     }
 }
