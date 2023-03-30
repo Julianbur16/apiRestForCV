4871cf786fc6edcbdd681566aea7bc9dc564c8c8
Alejandro Burbano <Julianbur16@gmail.com>
2023-03-30 03:51:36 -0500

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,6 @@ class Cors
     {
         $response= $next($request);  
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Credentials','true');
         $response->headers->set('Access-Control-Allow-Headers','Content-Type, Accept, Authorization, X-Requested-With, Application');
         $response->headers->set('Access-Control-Allow-Methods','PUT, POST, GET, OPTIONS, DELETE');
         return $response;
