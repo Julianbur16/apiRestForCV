e255bc0d4e4d7dc705373610621f4cb7a390c2be
Alejandro Burbano <Julianbur16@gmail.com>
2023-03-29 23:37:37 -0500

    add headers to api

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -34,6 +34,7 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
+        $request->headers->set('Accept', 'application/json');
         $clients=new client();
         $clients->name=$request->name;
         $clients->email=$request->email;
