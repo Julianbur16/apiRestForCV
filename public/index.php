911adb2d4841ea181f173b606cfbf043e200ad86
Alejandro Burbano <Julianbur16@gmail.com>
2023-03-30 01:53:53 -0500

    cors

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 /*
 |--------------------------------------------------------------------------
