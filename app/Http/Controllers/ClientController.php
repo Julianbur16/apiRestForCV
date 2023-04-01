e6c9a2d258cbd26fca2647234b58b5eef7876c06
Alejandro Burbano <Julianbur16@gmail.com>
2023-04-01 12:04:27 -0500

    wap messages

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -61,35 +61,53 @@ class ClientController extends Controller
         
         if($request->phone != ''){
 
-        $curl = curl_init();
+            $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://graph.facebook.com/v16.0/108950852159753/messages',
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS =>'{
-            "messaging_product": "whatsapp",
-            "to": 57'.$request->phone.',
-            "type": "text",
-            "text": { 
-            "preview_url": false,
-            "body": "Le confirmo la entrega de su mensaje, le daré pronta respuesta mediante email."
-        }
-        }',
-          CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json',
-            'Authorization: Bearer '.$token
-          ),
-        ));
-        
-        $response = curl_exec($curl);
-        
-        curl_close($curl);
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://graph.facebook.com/v16.0/108950852159753/messages',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'POST',
+              CURLOPT_POSTFIELDS =>'{
+                "messaging_product": "whatsapp",
+                "to": 57'.$request->phone.',
+                "type": "template",
+                "template": {
+                    "name": "mensage",
+                    "language": {
+                        "code": "es"
+                    },
+                    "components": [
+                        {
+                            "type": "body",
+                            "parameters": [
+                                {
+                                    "type": "text",
+                                    "text": "TEXT_STRING"
+                                    },
+                                
+                                {
+                                    "type": "text",
+                                    "text": "TEXT_STRING"
+                                    }
+                                ]
+                        }
+                    ]
+                }
+            }',
+              CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer '.$token
+            ),
+            ));
+            
+            $response = curl_exec($curl);
+            
+            curl_close($curl);
     }
     $curl = curl_init();
 
