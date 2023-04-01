c4689035708855ab920d508ef905fb4cc10853d7
Alejandro Burbano <Julianbur16@gmail.com>
2023-04-01 14:41:30 -0500

    sql type text for too long url

--- database/migrations/2023_03_27_143253_create_services_table.php
+++ database/migrations/2023_03_27_143253_create_services_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->Text('url')->unique();
-            $table->Text('description');
+            $table->text('url')->unique();
+            $table->text('description');
             $table->integer('type');
             $table->timestamps();
         });
