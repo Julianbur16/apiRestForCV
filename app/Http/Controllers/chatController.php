f614a19ad9b936c0365c7e52f14cdd77d4ed75f8
Alejandro Burbano <Julianbur16@gmail.com>
2023-04-04 17:49:10 -0500

    chat and table multilanguage

--- app/Http/Controllers/chatController.php
+++ app/Http/Controllers/chatController.php
@@ -17,7 +17,7 @@ class chatController extends Controller
             ],
             'json' => [
                 'model' => 'davinci',
-                'prompt' => 'Hello, how are you?',
+                'message' => 'Hello, how are you?',
                 'temperature' => 0.7,
                 'max_tokens' => 50,
                 'stop' => '\n'
